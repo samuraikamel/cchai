c1836dd68ec992208e1d5f383a5b4b8d46e7dbfd
samuraikamel <176351360+samuraikamel@users.noreply.github.com>
2024-10-08 12:46:09 -0700

    add library files

---
+++ src/cchai.c
@@ -0,0 +1,14 @@
+#include "cchai.h"
+#include <assert.h>
+
+struct Screen new_screen()
+{
+	struct Screen screen = {};
+
+	return screen;
+}
+
+void run_screen(const struct Screen* screen)
+{
+
+}
---
+++ src/cchai.h
@@ -0,0 +1,7 @@
+#pragma once
+
+struct Screen {};
+
+struct Screen new_screen();
+
+void run_screen(const struct Screen *screen);
